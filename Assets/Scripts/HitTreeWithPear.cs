c8811ca3b7c7c4a65fec75c59383e3550beb85fb
timitoch <saskesaske44@gmail.com>
2024-04-30 00:12:58 +0300

    7

---
+++ Assets/Scripts/HitTreeWithPear.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTreeWithPear : MonoBehaviour
+{
+    Animator animator;
+    public int treeHp = 2;
+    public Player player;
+    public float rage = 0.5f;
+    float distance;
+    public GameObject pearPrefab; // Префаб 
+
+    void Start()
+    {
+        player = FindAnyObjectByType<Player>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        CheckForHit();
+    }
+
+    void CheckForHit()
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        if (Input.GetKeyDown(KeyCode.Q) && rage >= distance && treeHp >= 1)
+        {
+            treeHp--;
+            animator.SetTrigger("Hit_tree");
+            StartCoroutine(SpawnApple());
+        }
+
+        if (treeHp < 1)
+        {
+            animator.SetTrigger("Falled_tree");
+        }
+    }
+
+    IEnumerator SpawnApple()
+    {
+        yield return new WaitForSeconds(2);
+        Instantiate(pearPrefab);
+    }
+}
